e73609c371f9a14c51dfd2ddee3d2e27b0b5a2f2
FelixBand <felixband@gmail.com>
2024-12-22 17:29:24 +0100

    Updated project - 2024-12-22 17:29:24

--- push-changes.bat
+++ push-changes.bat
@@ -9,9 +9,16 @@ ren "Five Nights at Freddy's 2.zip" "Five Nights at Freddy's 2.sb3"
 
 git add .
 
-for /f "tokens=1-4 delims=-: " %%a in ("%date% %time%") do (
-    set commit_msg=Updated project - %%a-%%b-%%c %%d
-)
+REM Commit changes with a timestamp
+for /f "tokens=2 delims==" %%a in ('"wmic OS Get localdatetime /value"') do set datetime=%%a
+set year=%datetime:~0,4%
+set month=%datetime:~4,2%
+set day=%datetime:~6,2%
+set hour=%datetime:~8,2%
+set minute=%datetime:~10,2%
+set second=%datetime:~12,2%
+
+set commit_msg=Updated project - %year%-%month%-%day% %hour%:%minute%:%second%
 
 git commit -m "%commit_msg%"
 
