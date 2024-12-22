b3dc00f63abaf9fb14d73b8b1920557d04093aff
FelixBand <felixband@gmail.com>
2024-12-22 17:27:16 +0100

    Updated project - 22/12/2024-17-27 16,34

--- push-changes.bat
+++ push-changes.bat
@@ -7,4 +7,16 @@ powershell -NoProfile -Command ^
 
 ren "Five Nights at Freddy's 2.zip" "Five Nights at Freddy's 2.sb3"
 
+git add .
+
+for /f "tokens=1-4 delims=-: " %%a in ("%date% %time%") do (
+    set commit_msg=Updated project - %%a-%%b-%%c %%d
+)
+
+git commit -m "%commit_msg%"
+
+git push || (echo Push failed. & exit /b 1)
+
+echo Changes successfully pushed!
+
 pause
